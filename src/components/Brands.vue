fef8458ad12b86b693b0444a76a21800b5209af6
Odumz <odumz20@gmail.com>
2022-02-01 09:18:03 +0100

    brand component created

---
+++ src/components/Brands.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Home",
+//   setup() {},
+});
+</script>
+
+<template lang="en">
+    <div class="grid grid-cols-6 bg-dark-purple text-light-purple">
+        Hi
+    </div>
+</template>
