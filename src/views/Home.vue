58a47c0602c36d1095bcd2ea9ba6e96d12d3c49d
Odumz <odumz20@gmail.com>
2022-02-01 09:17:06 +0100

    modifications made for basic auth app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Brands />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Brands from "@/components/Brands.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    Brands,
   },
 });
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `hsla(var(${variableName}), ${opacityValue})`;
+    }
+    return `hsl(var(${variableName}))`;
+  };
+}
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-purple": withOpacity("--color-dark-purple"),
+        "light-purple": withOpacity("--color-light-purple"),
+      },
+    },
   },
   plugins: [],
 };
